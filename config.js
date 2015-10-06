29eb392ffc651def7cc98a751bc6c5af5fbbbb9e
npsr <napster1321@gmail.com>
2015-10-06 12:11:38 +0200

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    wykop: {
-        appKey: 'GRAenylhFg',
-        appSecret: 'amy7TTrvpy',
-        host: 'a.wykop.pl',
-        accountkey: 'Lt5qmqOO7tPZ2IXX1QFu',
-        login: 'npsr'
-    }      
-}
